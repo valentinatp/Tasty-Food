63752d150a03422aaf0ce76af681a084de1f7e9a
Valentina Toledo <valentina.toledoporma@gmail.com>
2017-08-16 10:01:18 -0300

    base proyecto

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+
+
+gulp.task('script', function(){
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('style', function(){
+	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'assets/sass/main.scss'])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(minifyCSS())
+	.pipe(concat('style.min.css'))
+	.pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('assets/sass/*.scss', ['style']);
+});
+
+gulp.task('default', ['script', 'style', 'watch']);
