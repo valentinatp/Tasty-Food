00a5ddda66fe55dd07e8ac8aecbe48560d9b29e6
Valentina Toledo <valentina.toledoporma@gmail.com>
2017-08-17 09:51:46 -0300

    camnbios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-main.js
+$(document).ready(function(){
+   $(".button-collapse").sideNav();
+
+   var ajaxZomato = function(categorias){
+        $.ajax({
+        url: 'https://developers.zomato.com/api/v2.1/categories',
+       type: 'GET',
+       dataType: 'json',
+       headers: { 'user-key': '022db00cbdf26d706981d4fa3235767a' },
+        })
+        .done(function(response){
+            console.log(response);
+        })
+        .fail(function(){
+            console.log("error");
+        })
+    }
+
+    ajaxZomato(73);
+
+});
