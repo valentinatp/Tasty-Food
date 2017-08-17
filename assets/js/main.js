a43bebbad127140f5868da5841e635795e3548e9
Gilia Gongora <giliagongora.g@gmail.com>
2017-08-16 23:46:06 -0300

    Desarrollo profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-main.js
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+
+
+
+	function saveToLocalStorage() {
+        localStorage.setItem('photo', photo_url);      
+        localStorage.setItem('name', $('#name').val());
+        localStorage.setItem('email', $('#email').val());
+    }
+
+
+	function getFromLocalStorage() {
+    console.log('getting info for: ' + localStorage.getItem('#photo'));
+    $('#photo').attr('src', localStorage.getItem('photo'));
+    $('#name').val(localStorage.getItem('name'));
+    $('#email').val(localStorage.getItem('username'));
+}
+
+
+
+$(function(){
+	$("#profile_image").change(function(e){
+		var img = URL.createObjectURL(e.target.files[0]);
+		$(".image").attr("src",img);
+	})
+})
+
+});
